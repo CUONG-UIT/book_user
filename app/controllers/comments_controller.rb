ab2014ba2eb8f63f93c111e50f17017368d21fa9
CUONGUIT <14520114@gm.uit.edu.vn>
2018-03-21 07:09:36 +0700

    Comment code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,23 @@
 class CommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
+    #find book throught book_id
     @comment = @book.comments.create(comment_params)
+    #with each book ,we can create many comments
 
     redirect_to book_path(@book)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
+    #find book through book_id
     @comment = @book.comments.find(params[:id])
+    #with each book we find comment to delete
 
     @comment.destroy
     redirect_to book_path(@book)
+    #when comment was delete it'll go to books#show
+
   end
 
   private
--- db/migrate/20180319102844_add_category_id_to_books.rb
+++ db/migrate/20180319102844_add_category_id_to_books.rb
@@ -1,5 +1,7 @@
 class AddCategoryIdToBooks < ActiveRecord::Migration[5.1]
   def change
     add_column :books, :category_id, :integer
+
+    #add category_id column into Book table
   end
 end
